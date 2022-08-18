d6b9728143a488a38dacb189cc444b8337bd16f7
francogaliotti <francogaliotti@gmail.com>
2022-08-18 15:43:13 -0300

    socket services

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import socketService from './services/socketService';
 
 function App() {
+
+  const connectSocket = async () => {
+    const socket = await socketService
+      .connect("http://localhost:9000")
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
+  useEffect(() => {
+    connectSocket();
+  }, []);
+
   return (
     <div className="App">
 
---
+++ react-client/src/services/gameService/index.ts
@@ -0,0 +1,43 @@
+import { Socket } from "socket.io-client";
+//import { ILostMessage, IPlayMatrix, IStartGame } from "../../components/game";
+
+class GameService {
+  public async joinGameRoom(socket: Socket, roomId: string): Promise<boolean> {
+    return new Promise((rs, rj) => {
+      socket.emit("join_game", { roomId });
+      socket.on("room_joined", () => rs(true));
+      socket.on("room_join_error", ({ error }) => rj(error));
+    });
+  }
+
+  public async leaveGameRoom(socket: Socket, roomId: string): Promise<boolean> {
+    return new Promise((rs, rj) => {
+      socket.emit("leave_game", { roomId });
+      socket.on("room_leaved", () => rs(true));
+    });
+  }
+
+  /*public async updateGame(socket: Socket, gameMatrix: IPlayMatrix) {
+    socket.emit("update_game", { matrix: gameMatrix });
+  }
+
+  /*public async onGameUpdate(
+    socket: Socket,
+    listiner: (matrix: IPlayMatrix) => void
+  ) {
+    socket.on("on_game_update", ({ matrix }) => listiner(matrix));
+  }
+
+  public async onStartGame(
+    socket: Socket,
+    listiner: (options: IStartGame) => void
+  ) {
+    socket.on("start_game", listiner);
+  }
+
+  public async aloneInRoom(socket: Socket, listiner: () => void){
+    socket.on("alone_in_room", listiner)
+  }*/
+}
+
+export default new GameService();
